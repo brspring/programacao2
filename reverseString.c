bb655cebe6dd9439fcb4269b5356f0746ca36b8d
bruno <bruno@pop-os.localdomain>
2023-03-25 23:46:44 -0300

    exercicio 1 revertendo string

---
+++ reverseString.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#define SIZE 100
+int main(){
+    char str[SIZE+1]; //+1 pois tem o /0 no final da string
+
+    printf("Digite a string que quer imprimir o contrário.\n");
+
+    fgets (str, 100, stdin);
+
+    for (int strlen(str); i >= 0; i--){
+        putchar(str[i]);
+    }
+}
