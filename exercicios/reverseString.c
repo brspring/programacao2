be46e1ce02ee062399c67aeb81d1b2045e007756
bruno <brunoazizsm@gmail.com>
2023-03-27 16:21:29 -0300

    mudei o for do programa q imprime ocon

--- exercicios/reverseString.c
+++ exercicios/reverseString.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #define SIZE 100
+
 int main(){
     char str[SIZE+1]; //+1 pois tem o /0 no final da string
 
@@ -8,7 +9,8 @@ int main(){
 
     fgets (str, 100, stdin);
 
-    for (int strlen(str); i >= 0; i--){
+    for(int i = strlen(str)-1; i >= 0; i--){
         putchar(str[i]); 
     }
+    printf("\n");
 }
