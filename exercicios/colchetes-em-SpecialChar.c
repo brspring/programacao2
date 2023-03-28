3f6dabf21806eceba14cf91d99253aeba2707dac
bruno <bruno@pop-os.localdomain>
2023-03-28 08:42:15 -0300

    aa

--- exercicios/colchetes-em-SpecialChar.c
+++ exercicios/colchetes-em-SpecialChar.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+
 #define SIZE 100
 
-int main(){
+int main() {
     char str[SIZE+1];
-    int i,j;
+    int i, j;
+    char temp;
 
-    fgets(str, SIZE, stdin);
-    i = 0;
-    while (str[i] != '\0')                
-    {
-        if (!isalnum(str[i]) && str[i] != ' '){ 
-            j = i;
-            while (str[j] != '\0'){
-                str[j+1] = str[j+2];
-                j++;
+    printf("Digite uma string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Adiciona colchetes ao redor do caractere 'A'
+    for (i = 0; str[i] != '\0'; i++) {
+        temp = str[i];
+        if (!isalnum(str[i]) && str[i] != ' ' && str[i] != '\0' && str[i] != '\n') {
+            for (j = strlen(str); j > i; j--) {
+                str[j+2] = str[j];
             }
-        } else {                    
-        i++ ;
+            str[i] = '[';
+            str[i+1] = temp;
+            str[i+2] = ']';
+            i += 2;
         }
     }
 
-    printf("String modificada: %s\n", str);
+    printf("String resultante: %s\n", str);
+
+    return 0;
 }
