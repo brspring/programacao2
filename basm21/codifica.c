7a63d8f3c84540474548e7361bd1cf0db5fdf84b
bruno <bruno@pop-os.localdomain>
2023-04-16 17:30:59 -0300

    esta criando a tabela

--- basm21/codifica.c
+++ basm21/codifica.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 #include "liblista.h"
 
 #define TAM_MAX_LINHA 100
@@ -10,7 +11,7 @@
 int NumPalavrasLivro(FILE *livro)
 {
     char *string = malloc(sizeof(char) * TAM_MAX_LINHA);
-    int cont = 0;
+    int cont = 1;
 
     while (fgets(string, TAM_MAX_LINHA, livro))
     {
@@ -22,52 +23,84 @@ int NumPalavrasLivro(FILE *livro)
             }
         }
     }
+    rewind(livro);
     free(string);
     return cont;
 }
 
 int main(int argc, char *argv[])
 {
-    const char *livro_cifra = "livro.txt";
     FILE *livro;
-    livro = fopen(livro_cifra, "r");
-
-    if (!livro)
+    livro = fopen("livro.txt", "r+");
+    if (livro == NULL)
     {
         perror("Erro em abrir o arquivo!\n");
         return 1;
     }
 
-    //nao sei se vou usar, mas coloca em uma variavel o numerod e palavras do livro
+    // nao sei se vou usar, mas coloca em uma variavel o numerod e palavras do livro
     int num_palavras = NumPalavrasLivro(livro);
     printf("O arquivo tem %d palavras.\n", num_palavras);
 
-    //vetor que cada indice eh um caracter da tabela ASCII e ele eh do tipo lista para ter uma lista com a frequencia de cada letra
-    nodo_l_t *primeiraLetra[TAM_ASCII] = {NULL};
+    // vetor que cada indice eh um caracter da tabela ASCII e ele eh do tipo lista para ter uma lista com a frequencia de cada letra
+    nodo_l_t *vetorASCII[128] = {NULL};
 
-    int posicao = 0;
     char c;
 
-     while ((c = fgetc(livro)) != EOF) {
-        if (isascii(c)) {
+    int posicao = 0;
+    bool primeiraLetra = true; // variável para verificar se é a primeira letra de uma palavra
+    while ((c = fgetc(livro)) != EOF)
+    {
+        c = tolower(c);
+        if (isalpha(c))
+        {
             int index_pos = c;
-            nodo_l_t* novo_nodo = (nodo_l_t*) malloc(sizeof(nodo_l_t));
-            novo_nodo->posicao = posicao;
-            novo_nodo->prox = NULL;
 
-            if (primeiraLetra[index_pos] == NULL) {
-                primeiraLetra[index_pos] = novo_nodo;
-            } else {
-                nodo_l_t* current = primeiraLetra[index_pos];
-                while (current->prox != NULL) {
-                    current = current->prox;
+            if (primeiraLetra)
+            {
+                if (vetorASCII[index_pos] == NULL)
+                {
+                    vetorASCII[index_pos] = (nodo_l_t *)malloc(sizeof(nodo_l_t));
+                    vetorASCII[index_pos]->elemento = posicao;
+                    vetorASCII[index_pos]->prox = NULL;
                 }
-                current->prox = novo_nodo;
+                else
+                {
+                    nodo_l_t *current = vetorASCII[index_pos];
+                    while (current->prox != NULL)
+                    {
+                        current = current->prox;
+                    }
+                    current->prox = (nodo_l_t *)malloc(sizeof(nodo_l_t));
+                    current->prox->elemento = posicao;
+                    current->prox->prox = NULL;
+                }
+
+                primeiraLetra = false; // atualiza para que a próxima letra não seja a primeira de uma palavra
             }
         }
+        else if (c == ' ' || c == '\n')
+        {
+            primeiraLetra = true; // atualiza para que a próxima letra seja a primeira de uma palavra
+            posicao++;            // atualiza a posição
+        }
+    }
 
-        if (c == ' ' || c == '\n') {
-            posicao++;
+    rewind(livro);
+
+    for (int i = 0; i < 128; i++)
+    {
+        if (vetorASCII[i] != NULL)
+        {   
+            //printf("Lista para letra '%c' criada com sucesso!\n", i);
+            printf("Letra '%c': ", i);
+            nodo_l_t *current = vetorASCII[i];
+            while (current != NULL)
+            {
+                printf("%d ", current->elemento);
+                current = current->prox;
+            }
+            printf("\n");
         }
     }
 
--- basm21/liblista.h
+++ basm21/liblista.h
@@ -14,6 +14,7 @@
 
 struct nodo_l {
     int elemento;       /* lista de numeros inteiros */
+    int posicao;
     struct nodo_l* prox;  /* ponteiro para o proximo   */
 };
 typedef struct nodo_l nodo_l_t;
@@ -23,6 +24,7 @@ struct lista {
     int tamanho;        /* numero de elementos na lista    */
 };
 typedef struct lista lista_t;
+
 
 /*
  * Cria uma lista vazia e a retorna, se falhar retorna NULL.
