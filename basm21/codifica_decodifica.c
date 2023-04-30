3188654d196b47561c9b08606a96d990a5f49479
bruno <bruno@pop-os.localdomain>
2023-04-29 22:39:54 -0300

    so falta os free

--- basm21/codifica_decodifica.c
+++ basm21/codifica_decodifica.c
@@ -8,7 +8,7 @@
 #include "listaBeale.h"
 #include "codifica_decodifica.h"
 
-void cria_vetor_de_lista(FILE *arquivo, nodo_l_t **vetorASCII)
+void cria_vetor_de_lista_com_livro(FILE *arquivo, nodo_l_t **vetorASCII)
 {
     char c;
     int posicao = 0;
@@ -76,7 +76,6 @@ void cria_arquivo_de_chaves(nodo_l_t **vetorASCII)
             fprintf(ArquivoDeChaves, "\n");
         }
     }
-    printf("arquivo craido!\n");
     fclose(ArquivoDeChaves);
 }
 
@@ -112,14 +111,22 @@ void Cria_arq_msg_codificada(FILE *arquivo, nodo_l_t **vetorASCII)
 }
 
 
-void decodifica_msg_arq_de_chaves(FILE* arquivo, nodo_l_t **vetorASCII) {
+void cria_arq_msg_decodificada(nodo_l_t **vetorASCII) {
     /*novo arquivo com a mensagem codificada para ser usado na decodificação*/
+    
+    FILE *mensagem_codificada = fopen("MensagemCodificada.txt", "r");
+    if (!mensagem_codificada)
+    {
+        printf("Arquivo com a mensagem codificada não encontrado.\n");
+        exit(1);
+    }
+
     FILE* mensagem_decodificada = fopen("MensagemDecodificada.txt", "w");
 
     /*Decodifica a mensagem*/
     int char_codificado;
     
-    while(fscanf(arquivo, "%d", &char_codificado) != EOF){
+    while(fscanf(mensagem_codificada, "%d", &char_codificado) != EOF){
         if(char_codificado == -1)
         {
             fprintf(mensagem_decodificada, "%c" , 32);
@@ -129,6 +136,7 @@ void decodifica_msg_arq_de_chaves(FILE* arquivo, nodo_l_t **vetorASCII) {
         }
     }
     fclose(mensagem_decodificada);
+    fclose(mensagem_codificada);
 }
 
 void libera_vetor_de_lista(nodo_l_t **vetorASCII) {
@@ -145,4 +153,56 @@ void libera_vetor_de_lista(nodo_l_t **vetorASCII) {
         }
     }
     free(vetorASCII);
+}
+
+void cria_vetor_de_lista_com_arq_de_chaves(nodo_l_t **vetorASCII){
+    FILE *arquivo_de_chaves;
+    arquivo_de_chaves = fopen("ArquivoDeChaves.txt", "r");
+    
+    if (arquivo_de_chaves == NULL)
+    {
+        perror("Erro em abrir o arquivo de chaves!\n");
+        exit(1);
+    }
+
+   char linha[1000];
+
+    while (fgets(linha, 1000, arquivo_de_chaves) != NULL)
+    {
+        char* separador = strtok(linha, ": \n"); /* quebra a linha  com base nos : \n"*/
+
+        if (separador != NULL)
+        {
+            int indice = separador[0]; /*a primeira letra que é o indice do vetor*/
+            
+            separador = strtok(NULL, ": \n"); /*vai para o indice*/
+            
+            while (separador != NULL) /*vai por separadores*/
+            {
+                int posicao;
+                if (sscanf(separador, "%d", &posicao) == 1) /* verifica se o separador pode ser inteiro*/
+                {
+                    if (vetorASCII[indice] == NULL) /*se a lista da letra n existe*/
+                    {
+                        vetorASCII[indice] = (nodo_l_t*) malloc(sizeof(nodo_l_t));
+                        vetorASCII[indice]->elemento = posicao;
+                        vetorASCII[indice]->prox = NULL;
+                    }
+                    else /*se ja existe adiciona*/
+                    {
+                        nodo_l_t* atual = vetorASCII[indice];
+                        while (atual->prox != NULL)
+                        {
+                            atual = atual->prox;
+                        }
+                        atual->prox = (nodo_l_t*) malloc(sizeof(nodo_l_t));
+                        atual->prox->elemento = posicao;
+                        atual->prox->prox = NULL;
+                    }
+                }
+                separador = strtok(NULL, ": \n"); /* avanca para o próximo separador*/
+            }
+        }
+    }
+    rewind(arquivo_de_chaves);
 }
--- basm21/codifica_decodifica.h
+++ basm21/codifica_decodifica.h
@@ -3,10 +3,12 @@
 
 void cria_arquivo_de_chaves(nodo_l_t **vetorASCII);
 
-void cria_vetor_de_lista(FILE *arquivo, nodo_l_t **vetorASCII);
+void cria_vetor_de_lista_com_livro(FILE *arquivo, nodo_l_t **vetorASCII);
 
-void Cria_msg_codificada(FILE *arquivo, nodo_l_t **vetorASCII);
+void Cria_arq_msg_codificada(FILE *arquivo, nodo_l_t **vetorASCII);
 
-void descodifica_msg_arq_de_chaves(FILE* arquivo, nodo_l_t **vetorASCII);
+void cria_arq_msg_decodificada(nodo_l_t **vetorASCII);
+
+void cria_vetor_de_lista_com_arq_de_chaves(nodo_l_t **vetorASCII);
 
 #endif
